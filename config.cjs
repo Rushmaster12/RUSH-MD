c17e1373f240e6f4d78ad95d38400f057620c6bd
Rushmaster12 <160727728+Rushmaster12@users.noreply.github.com>
2024-10-18 15:55:32 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "RUSH-M𝗗",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "263789622747",
+  OWNER_NAME: process.env.OWNER_NAME || "𝚪𝐔𝐒𝚮 𝚳𝚫𝐒𝚻𝚵𝚪☯️",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "917020728211",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
